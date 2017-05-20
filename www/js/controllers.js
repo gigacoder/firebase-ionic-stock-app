6daa3b34a8fe4297abeb01378d69188e5b96cc2d
Parul Mishra <brunocoder@yahoo.com>
2017-05-19 17:21:15 -0700

    google authentication added in login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -136,7 +136,40 @@ function($scope, $location, $stateParams, $firebaseObject, FBURL){
   };
 
 }])
+.controller('AccountCtrl', AccountCtrl);
+function LoginCtrl(Auth, $state) {
 
+  this.loginWithGoogle = function loginWithGoogle() {
+    Auth.$authWithOAuthPopup('google')
+    .then(function(authData) {
+      $state.go('tab.dash');
+    });
+  };
+
+}
+LoginCtrl.$inject = ['Auth', '$state'];
+
+function DashCtrl() {}
+
+function ChatsCtrl($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+}
+ChatsCtrl.$inject = ['$scope', 'Chats'];
+
+function ChatDetailCtrl($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+}
+ChatDetailCtrl.$inject = ['$scope', '$stateParams', 'Chats'];
+
+function AccountCtrl($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+}
+AccountCtrl.$inject = ['$scope'];
 
 
 
